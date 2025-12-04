6b2a342361f6e140a5ef905019cd1d71efb28bb7
eiralv <eirik.alvestad@bidbax.no>
2025-12-04 10:34:50 +0100

    something

---
+++ tofu/environments/test/eiralv/s3_bucket.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "iacws-eirik-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "test_bucket_versioning" {
+  bucket = aws_s3_bucket.test_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_kms_key" "kms_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test_bucket_encryption" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "test_bucket_pab" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
