48359c8711c59b9448d123641a4c65df5dd6e602
Sigve Grevling <sigvegrevling@gmail.com>
2025-12-04 10:23:47 +0100

    many important changes

---
+++ tofu/environments/test/sigodo/_backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket       = "iacws-state-test"
+    key          = "test/sigodo/terraform.tfstate"
+    region       = "eu-north-1"
+    profile      = "iacws"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
---
+++ tofu/environments/test/sigodo/s3_bucket.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "sigodo-uploads" {
+  bucket = "iacws-sigodo-uploads"
+
+  tags = {
+    Name        = "Blorpo"
+    Environment = "Test"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "sigodo-uploads" {
+  bucket = aws_s3_bucket.sigodo-uploads.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sigodo-uploads" {
+  bucket = aws_s3_bucket.sigodo-uploads.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "sigodo-uploads" {
+  bucket = aws_s3_bucket.sigodo-uploads.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+#   block_public_buckets = true
+}
